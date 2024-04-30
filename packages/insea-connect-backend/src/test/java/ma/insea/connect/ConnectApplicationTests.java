1380b39b832dbb5a329b32c9b4454d2aac0ab319
Earthyy <elardi2001@gmail.com>
2024-04-30 14:45:49 +0000

    chore(backend): init spring boot project

---
+++ packages/insea-connect-backend/src/test/java/ma/insea/connect/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.insea.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
