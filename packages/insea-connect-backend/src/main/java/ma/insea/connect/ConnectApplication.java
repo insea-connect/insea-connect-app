94102e4d14b02dbd186f6bf087fd10b64b76eb49
AmimiHamza <amimi1hamza@gmail.com>
2024-05-10 20:22:54 +0100

    fix : fix conflits

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/ConnectApplication.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/ConnectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-// import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ma.insea.connect.user.User;
 import ma.insea.connect.user.UserRepository;
@@ -20,16 +20,16 @@ public class ConnectApplication {
 		SpringApplication.run(ConnectApplication.class, args);
 	}
 
-	// @Profile("dev")
-	// @Bean
-	// public CommandLineRunner addAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-	// 	return args -> {
-	// 		User user = User.builder()
-	// 				.username("admin")
-	// 				.role(Role.STUDENT)
-	// 				.passwordHash(passwordEncoder.encode("admin"))
-	// 				.build();
-	// 		userRepository.save(user);
-	// 	};
-	// }
+	@Profile("dev")
+	@Bean
+	public CommandLineRunner addAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			User user = User.builder()
+					.username("admin")
+					.role(Role.STUDENT)
+					.passwordHash(passwordEncoder.encode("admin"))
+					.build();
+			userRepository.save(user);
+		};
+	}
 }
--- packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/model/Folder.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/model/Folder.java
@@ -1,21 +1,21 @@
-// package ma.insea.connect.drive.model;
+package ma.insea.connect.drive.model;
 
-// import jakarta.persistence.CascadeType;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.OneToMany;
-// import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-// @Entity
-// @Getter
-// @Setter
-// @AllArgsConstructor
-// @NoArgsConstructor
-// @Builder
-// public class Folder extends DriveItem {
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Folder extends DriveItem {
 
-//     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
-//     private List<DriveItem> children = new ArrayList<>();
-// }
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<DriveItem> children = new ArrayList<>();
+}
