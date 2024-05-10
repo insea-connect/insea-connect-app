a425edbb37592c1664d06764355664cbc2991c3c
AmimiHamza <amimi1hamza@gmail.com>
2024-05-10 19:45:01 +0100

    fix : recover login requirement

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/config/SecurityConfig.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -33,11 +34,12 @@ public class SecurityConfig {
                         .disable())
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .authorizeHttpRequests(
-                        req->req.requestMatchers("/**")
+                        req->req.requestMatchers("/login/**","/register/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 ).userDetailsService(userDetailsService)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
