2bdb437b0e1f075f236cebbec8ff8ac56748dcdd
AmimiHamza <amimi1hamza@gmail.com>
2024-05-19 19:18:59 +0100

    feat : add some chat and group messages

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/utils/DummyUserLoader.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/utils/DummyUserLoader.java
@@ -1,6 +1,7 @@
 package ma.insea.connect.utils;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,12 @@ import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import ma.insea.connect.chat.common.chatMessage.ChatMessage;
+import ma.insea.connect.chat.common.chatMessage.ChatMessageRepository;
+import ma.insea.connect.chat.common.chatMessage.GroupMessage;
+import ma.insea.connect.chat.common.chatMessage.GroupMessageRepository;
+import ma.insea.connect.chat.conversation.Conversation;
+import ma.insea.connect.chat.conversation.ConversationRepository;
 import ma.insea.connect.chat.group.Group;
 import ma.insea.connect.chat.group.GroupRepository;
 import ma.insea.connect.chat.group.Membership;
@@ -33,14 +40,17 @@ public class DummyUserLoader implements CommandLineRunner {
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
     private final MembershipRepository membershipRepository;
+    private final GroupMessageRepository groupMessageRepository;
+    private final ChatMessageRepository chatMessageRepository;
+    private final ConversationRepository conversationRepository;
 
 
     @Override
     public void run(String... args) throws Exception {
-        loadDummyUsers(userRepository,groupRepository,membershipRepository);
+        loadDummyUsers(userRepository,groupRepository,membershipRepository, groupMessageRepository,chatMessageRepository,conversationRepository);
     }
 
-    private void loadDummyUsers(UserRepository userRepository,GroupRepository groupRepository, MembershipRepository membershipRepository) {
+    private void loadDummyUsers(UserRepository userRepository,GroupRepository groupRepository, MembershipRepository membershipRepository,GroupMessageRepository groupMessageRepository , ChatMessageRepository chatMessageRepository,ConversationRepository conversationRepository) {
         AddUserDTO user = AddUserDTO.builder()
 					.username("anas")
 					.email("anas")
@@ -108,8 +118,56 @@ public class DummyUserLoader implements CommandLineRunner {
             // group.setMemberships(memberships);
             // membershipRepository.saveAll(memberships);
             groupRepository.save(group);
+
+            GroupMessage groupMessage1=new GroupMessage();
+            groupMessage1.setGroupId(group.getId());
+            groupMessage1.setContent("this is anas from the group");
+            groupMessage1.setTimestamp(new java.sql.Date(System.currentTimeMillis()));
+            groupMessage1.setSender(anas);
+            groupMessageRepository.save(groupMessage1);
+
+            GroupMessage groupMessage2=new GroupMessage();
+            groupMessage2.setGroupId(group.getId());
+            groupMessage2.setContent("welcome anas , this is soulayman");
+            groupMessage2.setTimestamp(new java.sql.Date(System.currentTimeMillis()));
+            groupMessage2.setSender(soulayman);
+            groupMessageRepository.save(groupMessage2);
+
+
+
+
+        ChatMessage chatMessage1 = new ChatMessage();
+        var chatId = getChatRoomId(Long.toString(anas.getId()),Long.toString(soulayman.getId()), true);
+        chatMessage1.setChatId(chatId);
+        chatMessage1.setSender(anas);
+        chatMessage1.setRecipient(soulayman);
+        chatMessage1.setContent("hello soulayman , my name is anas");
+        chatMessage1.setTimestamp(new Date());
+        chatMessageRepository.save(chatMessage1);
+
+        Conversation conversation = new Conversation();
+        conversation.setChatId(chatId);
+        conversation.setMember1(anas);
+        conversation.setMember2(soulayman);
+        conversationRepository.save(conversation);   
+        
+        ChatMessage chatMessage2 = new ChatMessage();
+        var chatId2 = getChatRoomId(Long.toString(anas.getId()),Long.toString(soulayman.getId()), true);
+        chatMessage2.setChatId(chatId2);
+        chatMessage2.setSender(soulayman);
+        chatMessage2.setRecipient(anas);
+        chatMessage2.setContent("nice to meet you anas , my name is soulayman");
+        chatMessage2.setTimestamp(new Date());
+        chatMessageRepository.save(chatMessage2);
         
     }
+    public String getChatRoomId(String senderId,String recipientId,boolean createNewRoomIfNotExists)
+         {
+            var first = senderId.compareTo(recipientId) < 0 ? senderId : recipientId;
+            var second = senderId.compareTo(recipientId) < 0 ? recipientId : senderId;
+            var chatId = String.format("%s_%s",first, second);
+            return chatId;
+}
 
     
 
