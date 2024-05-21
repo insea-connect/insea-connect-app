a0f677b72aafcb50c1ffac69b84e016f16280dbb
Earthyy <elardi2001@gmail.com>
2024-05-21 14:13:55 +0000

    chore: establish STOMP over WS connection

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/config/WebSocketConfig.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/config/WebSocketConfig.java
@@ -31,8 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins(allowedserver)
-                .withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins(allowedserver);
     }
 
     @Override
