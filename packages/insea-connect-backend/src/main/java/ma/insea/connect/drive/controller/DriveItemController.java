549299cd67ca0e8fc98921947b57e435310c2e94
Anas Mokhannate <mokhannateanas@gmail.com>
2024-05-21 14:52:39 +0100

    feat: add upload endpoint

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/controller/DriveItemController.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/controller/DriveItemController.java
@@ -1,17 +1,27 @@
 package ma.insea.connect.drive.controller;
 
 
+import lombok.RequiredArgsConstructor;
 import ma.insea.connect.drive.model.DriveItem;
+import ma.insea.connect.drive.model.File;
+import ma.insea.connect.drive.model.Folder;
 import ma.insea.connect.drive.service.DriveItemServiceImpl;
+import ma.insea.connect.utils.Functions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/drive")
+@RequiredArgsConstructor
 public class DriveItemController {
+
+    private static final String UPLOAD_DIR = "uploads";
+    private final Functions functions;
 
     @Autowired
     private DriveItemServiceImpl driveItemService;
@@ -26,11 +36,26 @@ public class DriveItemController {
         return ResponseEntity.ok(driveItemService.getDriveItems(degreePathCode));
     }
 
-    @PostMapping("/degreePaths/{degreePathCode}/items")
-    public ResponseEntity<DriveItem> CreateDriveItem(@PathVariable Long degreePathCode, @RequestBody DriveItem driveItem) {
-        if (driveItemService.createDriveItem(degreePathCode, driveItem) == null) {
+    @PostMapping("/degreePaths/{degreePathCode}/folder")
+    public ResponseEntity<Folder> CreateDriveItem(@PathVariable Long degreePathCode, @RequestBody Folder folder) {
+        if (driveItemService.createDriveItem(degreePathCode, folder) == null) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(driveItem);
+        return ResponseEntity.ok(folder);
+    }
+
+    @PostMapping("/degreePaths/{degreePathCode}/upload")
+    public File handleFileUpload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {return null;}
+
+        File fileObj = new File();
+        fileObj.setFileUrl(functions.uploadFile(file));
+        fileObj.setName(file.getOriginalFilename());
+        fileObj.setSize(file.getSize());
+        fileObj.setMimeType(file.getContentType());
+        fileObj.setCreatedAt(LocalDateTime.now());
+
+        return fileObj;
+
     }
 }
--- packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/controller/FolderController.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/controller/FolderController.java
@@ -31,23 +31,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FolderController {
 
-    private static final String UPLOAD_DIR = "uploads";
-    private final Functions functions;
-
 
     @Autowired
     private FolderServiceImpl folderService;
-
-    @PostMapping("/upload")
-    public File handleFileUpload(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {return null;}
-
-            // String fileName = file.getOriginalFilename();
-            String fileType = file.getContentType();
-            Long fileSize = file.getSize();
-            String filePath = functions.uploadFile(file);
-            return new File(fileSize,fileType, filePath); 
-    }
 
 
     @GetMapping("/{folderId}/items")
