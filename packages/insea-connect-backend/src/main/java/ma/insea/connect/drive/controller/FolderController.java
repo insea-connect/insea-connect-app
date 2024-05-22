71f3693f83878eac8bef1b2d44e5a869c58b7227
AmimiHamza <amimi1hamza@gmail.com>
2024-05-22 21:25:11 +0100

    fix : remove unused imports

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/controller/FolderController.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/drive/controller/FolderController.java
@@ -1,7 +1,6 @@
 package ma.insea.connect.drive.controller;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
