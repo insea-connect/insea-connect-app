696a124a74ea6c7cec4a990398724a72001c94a1
AmimiHamza <amimi1hamza@gmail.com>
2024-05-23 07:39:52 +0100

    feat : add feauture for online based on heartbeat mechanism

---
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/user/OnlineDTO.java
@@ -0,0 +1,11 @@
+package ma.insea.connect.user;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+public class OnlineDTO {
+    private Status status;
+    private Date lastLogin;
+
+}
