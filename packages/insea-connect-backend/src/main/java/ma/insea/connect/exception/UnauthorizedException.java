6f212a3c4b9d928226eb0ea59307e8cd74a6409a
AmimiHamza <amimi1hamza@gmail.com>
2024-05-23 04:53:49 +0100

    feat : handle exception of managing admins

---
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/exception/UnauthorizedException.java
@@ -0,0 +1,14 @@
+package ma.insea.connect.exception;
+
+// Add Exception Handling Status
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+    
+}
