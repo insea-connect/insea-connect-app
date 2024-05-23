3545c0247ee4933d9d0bf87f239535befadf50ae
AmimiHamza <amimi1hamza@gmail.com>
2024-05-23 04:06:19 +0100

    fix:add constructors

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/chat/group/GroupDTO2.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/chat/group/GroupDTO2.java
@@ -1,9 +1,13 @@
 package ma.insea.connect.chat.group;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ma.insea.connect.chat.common.chatMessage.GroupMessageDTO;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupDTO2 {
     private Long id;
     private String name;
