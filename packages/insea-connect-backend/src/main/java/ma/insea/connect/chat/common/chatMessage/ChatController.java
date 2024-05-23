6c4c72f5b45b377ee664cd5e4ecd0b6b814a6a17
AmimiHamza <amimi1hamza@gmail.com>
2024-05-23 05:59:03 +0100

    feat: add typing feature

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/chat/common/chatMessage/ChatController.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/chat/common/chatMessage/ChatController.java
@@ -2,6 +2,9 @@ package ma.insea.connect.chat.common.chatMessage;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +23,13 @@ public class ChatController {
     @MessageMapping("/sendgroupmessage")
     public GroupMessage processGroupMessage(@RequestBody GroupMessageDTO groupMessage) {
         return chatMessageService.savegroupmessage(groupMessage);
-         
+    }
+    @MessageMapping("/conversation/typing")
+    public ResponseEntity<TypingDTO> chatTyping(@RequestBody Map<String,Long> body) {
+        return ResponseEntity.ok(chatMessageService.chatTyping(body.get("recipientId")));
+    }
+    @MessageMapping("/group/Typing")
+    public ResponseEntity<TypingDTO> grouptyping(@RequestBody Map<String,Long> body) {
+        return ResponseEntity.ok(chatMessageService.groupTyping(body.get("groupId")));
     }
 }
