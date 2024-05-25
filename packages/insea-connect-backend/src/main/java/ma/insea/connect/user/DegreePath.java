56d8cf95edad1a453a5960e069fdbb46a9b6c4da
AmimiHamza <amimi1hamza@gmail.com>
2024-05-25 04:56:59 +0100

    feat: add addUser methode

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/user/DegreePath.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/user/DegreePath.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import ma.insea.connect.drive.model.DriveItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -24,10 +25,23 @@ public class DegreePath {
     private int pathYear;
 
     @OneToMany(mappedBy = "degreePath")
+    @JsonIgnore
     private List<User> students;
 
 
     @OneToMany(mappedBy = "degreePath")
     @JsonIgnore
     private List<DriveItem> driveItems;
+    public DegreePath(String cycle, String major, int pathYear) {
+        this.cycle = cycle;
+        this.major = major;
+        this.pathYear = pathYear;
+    }
+
+    public void addStudent(User student) {
+        if(this.students == null){
+            this.students = new ArrayList<>();
+        }
+        this.students.add(student);
+    }
 }
