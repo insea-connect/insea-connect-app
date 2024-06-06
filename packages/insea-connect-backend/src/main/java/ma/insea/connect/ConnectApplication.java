034a02b3c9b62628e99408651bc926304f4da52f
AmimiHamza <amimi1hamza@gmail.com>
2024-06-06 15:43:46 +0100

    fix: add enable methode security

--- packages/insea-connect-backend/src/main/java/ma/insea/connect/ConnectApplication.java
+++ packages/insea-connect-backend/src/main/java/ma/insea/connect/ConnectApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-
+@EnableMethodSecurity
 public class ConnectApplication {
 
 	public static void main(String[] args) {
